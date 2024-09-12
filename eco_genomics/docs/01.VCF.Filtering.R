6232f3aca20c6c2048768ed49e911f5708fe412a
Miles Garvin (they/he) <mgarvin1@node302.cluster>
2024-09-12 17:10:10 -0400

    Finished for 9/12 class

--- eco_genomics/docs/01.VCF.Filtering.R
+++ eco_genomics/docs/01.VCF.Filtering.R
@@ -8,3 +8,15 @@ list.files() #basically ls
 #read vcf files
 vcf <- read.vcfR("/gpfs1/cl/pbio3990/PopulationGenomics/variants/Centaurea_filtered.vcf.gz")
 head(vcf)
+
+#get reference genome
+dna <- ape::read.dna("/gpfs1/cl/pbio3990/PopulationGenomics/reference/GCA_030169165.1_ASM3016916v1_genomic.fa.gz", format="fasta")
+
+#annotation
+gff <- read.table("/gpfs1/cl/pbio3990/PopulationGenomics/reference/GCA_030169165.1_ASM3016916v1_genomic_genes.gff.gz", sep="\t", quote="")
+
+
+#Put them all together for chromosome 1
+chr1 <- create.chromR(name="Chromosome 1", vcf=vcf, seq=dna, ann=gff)
+###Makes a plot!!1
+plot(chr1)
